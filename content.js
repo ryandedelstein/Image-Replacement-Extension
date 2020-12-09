d97e94769924ada5929d4e7ed291c06dac082a9f
ryandedelstein <ryan.d.edelstein@vanderbilt.edu>
2020-12-09 10:44:13 -0500

    Pres 12/9

--- content.js
+++ content.js
@@ -1,16 +1,27 @@
 chrome.storage.sync.get({
+
     value : "Reset"
+
 }, function(fields) {
     if (fields.value == "Replace Images") {
-        //this is the code that will be run whenever 
+
+        //this is the code that will be run whenever the button is clicked
+        //*Start Modifications */
         var imgs = document.getElementsByTagName("img");
-        var url = "https://corporate.comcast.com/media/img/original/2019/02/corporate_Official-Comcast-Logo.png";
+
+        var url = "https://i.ibb.co/LNNbPbL/Casey.jpg";
+
         window.setInterval(function() {
+
             for (img of imgs) {
+
                 img.src = url;
+                
             }
         }, 1);
+        //*End Modifications */
     } else {
+        //when reset is clicked it clears what was done before
         window.clearInterval();
     }
 });
